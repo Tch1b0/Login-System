2ed78f3ee2df3eed6d98071f9f86bef3c1f427fa
Tch1b0 <Jpour3006@gmail.com>
2020-01-12 12:30:23 +0100

    trying to make able to sign up

--- Login-system.py
+++ Login-system.py
@@ -6,24 +6,40 @@ os.chdir(r'C:\Users\User\Documents\Pgit\Login-system')
 info = open('information.txt', 'r')
 
 Password = False
+Login = True
+register = False
 
 info_contents = info.read()
 
-Username = input("Username?\n")
-if Username in info_contents:
-    sleep (1)
-    Password = True
-else:
-    print ("You are not regitstered")
-
-while Password == True:
-    Password_input = input("Password\n")
-    Password_in = ("("+str(Password_input)+")")
-    if Password_in in info_contents:
-        print ("You are now logged in")
-        quit()
+while Login:
+    Username_input = input("Username:\n")
+    Username = ("{"+str(Username_input)+"}")
+    if Username in info_contents:
+        sleep (1)
+        Password = True
     else:
-        print("wrong password")
+        print ("You are not regitstered")
+        sleep (1)
+        print ("Do you want to register?")
+        register_answer = input()
+        if register_answer is "yes"or"y":
+            register = True
+
+    while Password == True:
+        Password_input = input("Password:\n")
+        Password_in = ("("+str(Password_input)+")")
+        if Password_in in info_contents:
+            print ("You are now logged in")
+            quit()
+            info.close()
+        else:
+            print("wrong password")
+    
+    while register:
+        info.close
+        quit()
+
+
 
 info.close()
 quit()
