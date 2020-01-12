ebad54ab799ffa2ecf93abbeec228423dbaca461
Tch1b0 <Jpour3006@gmail.com>
2020-01-12 11:28:51 +0100

    tried to let the program read the other file

--- Login-system.py
+++ Login-system.py
@@ -1,2 +1,20 @@
 from time import sleep
+import os
 
+os.chdir(r'C:\Users\User\Documents\Pgit\Login-system')
+
+info = open('information.txt', 'r')
+
+Login = True
+while Login:
+    print ("Username:\n")
+    Username = input()
+    if Username.lower() in info.read():
+        print ("You are on the list!")
+        info.close()
+    elif Username.lower() not in info.read():
+        print ("You are not registered right now!")
+        info.close()
+        quit()
+
+info.close()
