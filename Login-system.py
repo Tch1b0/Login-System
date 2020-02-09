ac732ae498e81b5ca97807a20a0e55e177c662c4
Tch1b0 <Jpour3006@gmail.com>
2020-02-09 10:02:05 +0100

    The program closes if you enter 3 times a wrong pw

--- Login-system.py
+++ Login-system.py
@@ -6,21 +6,22 @@ os.chdir(current_path)                                       #the current path g
 
 info = open('information.txt', 'r+')
 
-Password = False                                      #the password activity is 'False' because you fist need to sign in your username
-Login = True                                          
-Register1 = False                                     #Register is off at the beginning
-Register2 = False                                     #Register is off at the beginning
-Error = False
+password = False                                      #the password activity is 'False' because you fist need to sign in your username
+login = True                                          
+register1 = False                                     #Register is off at the beginning
+register2 = False                                     #Register is off at the beginning
+error = False
+Wrong_Password = 0
 
 info_contents = info.read()                           
 
-while Login:
-    Username_input = input("Username:\n")             #the username gets asked
-    Username = ("{"+str(Username_input.lower())+"}")    #the username gets two curly brackets to distinguish between username and password
+while login:
+    username_input = input("Username:\n")             #the username gets asked
+    username = ("{"+str(username_input.lower())+"}")    #the username gets two curly brackets to distinguish between username and password
 
-    if Username in info_contents:                     #if the username is in the 'information.txt'... 
+    if username in info_contents:                     #if the username is in the 'information.txt'... 
         sleep (1)           
-        Password = True                               #... the password sequence gets activated
+        password = True                               #... the password sequence gets activated
     else:                                             #if the username doesn't exist...
         print ("You are not registered")             #... you get asked if you want to sign up
         sleep (1)
@@ -32,34 +33,37 @@ while Login:
             info.close()
             quit()
 
-    while Password == True:
-        Password_input = input("Password:\n")         #here the program asks for your password
-        Password_in = (str(Username)+"("+str(Password_input)+")") #the 'Password_input' gets two brackets to distinguish between username and password
-        if Password_in in info_contents:              #if the password is in the 'information.txt'...
+    while password == True:
+        if Wrong_Password == 3:
+            exit("access denied")
+        password_input = input("Password:\n")         #here the program asks for your password
+        password_in = (str(username)+"("+str(password_input)+")") #the 'Password_input' gets two brackets to distinguish between username and password
+        if password_in in info_contents:              #if the password is in the 'information.txt'...
             print ("You are now logged in")           #... you get logged in 
             info.close()
             quit()                             
         else:                                         #if the password can't get found in the 'information.txt'
             print("wrong password")                   #you have to try again
+            Wrong_Password = Wrong_Password + 1
     
     while register1:                                   #while regiser is active...
-        which_Username = input("Which Username do you want to have?\n") #... you choose a Username
-        if which_Username.lower() in info_contents:
+        which_username = input("Which Username do you want to have?\n") #... you choose a Username
+        if which_username.lower() in info_contents:
             print ("that name already is registered")
             press_any_key = input("press any key to move on\n")
-            Error = True
-        elif which_Username not in info_contents:
+            error = True
+        elif which_username not in info_contents:
             register2 = True
         while register2:
-            new_Username = ("{"+str(which_Username)+"}")            #... the username gets written in with curly brackets
-            info.write(new_Username.lower())                        #... the username gets written in the 'information.txt'
+            new_username = ("{"+str(which_username)+"}")            #... the username gets written in with curly brackets
+            info.write(new_username.lower())                        #... the username gets written in the 'information.txt'
             sleep (0.5) 
-            which_Password = input("Which passoword do you want to have?\n")#here you can choose your password
-            new_Password = ("("+str(which_Password)+")")            #the password gets brackets
-            info.write(new_Password.lower())
+            which_password = input("Which passoword do you want to have?\n")#here you can choose your password.
+            new_password = ("("+str(which_Password)+")")            #the password gets brackets
+            info.write(new_password.lower())
             info.write(".")
             sleep (1)
-            if Error:
+            if error:
                 print ("sorry, we couldn't set up your account.\n please try again.")
                 info.close()
                 quit()
